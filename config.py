cad15a6b32c9663c255b6ae1d9569e624b20f196
Douglas Miller <dougmiller@themetacity.com>
2014-03-06 11:39:53 +1000

    converting to PostgreSQL from SQLite. Added config file for DB poroduction settigns (and promptly ignored it on git, so you will need to make it on each server.) Changed thte models (enums specifically) to reflect the psql specific settings.

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
+from configdetails import SQLUSER, SQLPASSWORD, SQLDB, SQLHOST, SECRET_KEY
 import os
 
 CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
+SECRET_KEY = SECRET_KEY
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'tmcmedia.db')
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://'+SQLUSER+':'+SQLPASSWORD+'@'+SQLHOST+'/'+SQLDB
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
--- theMetaCityMedia/models.py
+++ theMetaCityMedia/models.py
@@ -18,9 +18,9 @@ class VideoFile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     parent_video = db.Column(db.Integer, db.ForeignKey('video.id'))
     extention = db.Column(db.String(4), default="webm")
-    video_codec = db.Column(db.Enum('mp4', 'ogv', 'webm'), default="webm")
-    audio_codec = db.Column(db.Enum('mp3', 'oga'), default="oga")
-    mime_type = db.Column(db.Enum('mp4', 'ogv', 'webm'), default="webm")
+    video_codec = db.Column(db.Enum('mp4', 'ogv', 'webm', name='video_codec'), default='webm')
+    audio_codec = db.Column(db.Enum('mp3', 'oga', name='audio_codec'), default='oga')
+    mime_type = db.Column(db.Enum('mp4', 'ogv', 'webm', name='mime_type'), default='webm')
     resolution = db.Column(db.String(16))
     is_fullscreen = db.Column(db.Boolean, default=False)
     has_fullscreen = db.Column(db.Boolean, default=False)
@@ -38,7 +38,7 @@ class VideoFile(db.Model):
 class Track(db.Model):
     __tablename__ = 'track'
     id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.Enum('subtitle', 'caption', 'description', 'chapters', 'metadata'))
+    type = db.Column(db.Enum('subtitle', 'caption', 'description', 'chapters', 'metadata', name='track_type'))
     src_lang = db.Column(db.String(16), default="en-AU")
     parent_video = db.Column(db.Integer, db.ForeignKey('video.id'))
 
