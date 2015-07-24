2c2454db3706f432a804dedf8d22c37f01125f14
Douglas Miller <dougmiller@themetacity.com>
2015-07-24 20:01:43 +1000

    Refectored the deployer to be more consistant and have better error messages.

--- deployer.sh
+++ deployer.sh
@@ -2,8 +2,8 @@
 set +e
 
 if [[ $# -ne 1 ]]; then
-  echo "Usage:"
-  echo "target   # deploy to target destination"
+  echo "Usage: target"
+  echo "Targets are 'develpment' and 'production'"
   exit -1
 fi
 
@@ -12,16 +12,15 @@ targets=("development", "production")
 # Add mounts as needed
 if ! [[ ${targets[*]} =~ $1 ]]; then
   echo "Unknown target. Please provide a known target."
-  echo "Known target are: 'development', 'production'"  # Have to update these manually as bash does not have nested mounts
+  echo "Known target are: 'development', 'production'"  # Have to update these manually as bash does not have nested arrays
   exit 2
 fi
-
 
 # Proper command found, time to execute it
 echo "I am going to deploy to target: $1"
 
 if [ $1 == "development" ]; then
-  echo "Starting deployment."
+  echo "Starting deployment to develpment environment"
   echo "Starting to copy files"
 
   cd theMetaCityMedia
@@ -32,22 +31,22 @@ if [ $1 == "development" ]; then
 elif [ $1 == "production" ]; then
   echo "Checking if mount available"
   if mountpoint -q /media/tmcmedia ; then
-      echo "Starting deployment"
-      echo "Starting to copy files"
-      cd theMetaCityMedia
-      cp -R * /media/tmcmedia/theMetaCityMedia
-      echo "Finished copying files"
-      echo "Changing strings to production values"
-      sed -i '/admin/d' /media/tmcmedia/theMetaCityMedia/__init__.py
-      cd /media/tmcmedia/theMetaCityMedia/templates
-
-      for i in *; do
-        sed -i 's/http:\/\/assets.localcity.com/https:\/\/assets.themetacity.com/g' ${i}
-      done
-
-      echo "Finished deployment"
-  else
     echo "tmcMedia is not mounted. Exiting."
     exit 3
   fi
+
+  echo "Starting deployment to production environment"
+  echo "Starting to copy files"
+  cd theMetaCityMedia
+  cp -R * /media/tmcmedia/theMetaCityMedia
+  echo "Finished copying files"
+  echo "Changing strings to production values"
+  sed -i '/admin/d' /media/tmcmedia/theMetaCityMedia/__init__.py
+  cd /media/tmcmedia/theMetaCityMedia/templates
+
+  for i in *; do
+    sed -i 's/http:\/\/assets.localcity.com/https:\/\/assets.themetacity.com/g' ${i}
+  done
+
+  echo "Finished deployment"
 fi
